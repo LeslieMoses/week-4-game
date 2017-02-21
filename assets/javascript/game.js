57ed141be9ed7cd380c1fab1f6d4f61a469d4488
Leslie Moses <LesMoses@gmail.com>
2017-02-21 11:34:07 -0500

    crystals 0221 1130a

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+
+//Game counters
+var winCounter = 0;
+var lossCounter = 0;
+
+//Counts 
+var million = 0;
+var yourTotal = 0;
+
+//Crystals' worth
+var blueWorth = 0;
+var whiteWorth = 0;
+var prupleWorth = 0;
+var pinkWorth = 0;
+
+//Restart
+var newGame = ??;
+
+
+
+//game start
+function startGame() {
+
+	
+}
